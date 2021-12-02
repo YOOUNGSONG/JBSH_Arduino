87ed0ecc8b0c61654c0dca1e9102af757e62a9ab
YOUNG <83584842+YOOUNGSONG@users.noreply.github.com>
2021-12-02 12:09:17 +0900

    Update LED.ino
    prevent overflow

--- LED.ino
+++ LED.ino
@@ -7,9 +7,15 @@ void setup() {
 void loop() {
   if(digitalRead(2) == HIGH){
     LigntIntensity = LigntIntensity + 10;
+    if(LigntIntensity > 255){
+      LigntIntensity = 255;
+    }
   }
   if(digitalRead(3) == HIGH){
     LigntIntensity = LigntIntensity - 10;
+    if(LigntIntensity < 0){
+      LigntIntensity = 0;
+    }
   }
   for(int i=9; i<12; i++){
     analogWrite(i, LigntIntensity);
