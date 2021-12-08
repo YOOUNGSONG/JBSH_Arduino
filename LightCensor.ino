dbe38852fbf916ff7338e883f894317a86285729
YOUNG <83584842+YOOUNGSONG@users.noreply.github.com>
2021-12-08 11:16:13 +0900

    Update LightCensor.ino
    Serco Code is Temporal.

--- LightCensor.ino
+++ LightCensor.ino
@@ -1,7 +1,14 @@
 #include <LiquidCrystal.h>
+#include <Servo.h>
+
 LiquidCrystal lcd(12, 11, 2, 3, 4, 5);
+Servo srvo1;
+Servo srvo2;
+
 void setup() {
   lcd.begin(16, 2);
+  srvo1.attach(9);
+  srvo2.attach(10);
 }
 
 void loop() {
@@ -9,5 +16,11 @@ void loop() {
   lcd.clear();
   lcd.print("Light: ");
   lcd.print(light);
+  for(int i=0; i<120; i++){
+    srvo1.write(i);
+    srvo2.write(i);
+    delay(15);
+  }
+  
   delay(100);
 }
